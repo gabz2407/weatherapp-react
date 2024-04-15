f9be24f4734522cbd317e32287dde66d4d83ccf1
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-15 15:05:27 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/gabriela-agostinho-378b67207/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Gabriela Agostinho
-        </a>{" "}
-        and it is open-sourced on{" "}
-        <a
-          href="https://github.com/gabz2407/weatherapp-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/gabriela-agostinho-378b67207/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gabriela Agostinho
+          </a>{" "}
+          and it is open-sourced on{" "}
+          <a
+            href="https://github.com/gabz2407/weatherapp-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
